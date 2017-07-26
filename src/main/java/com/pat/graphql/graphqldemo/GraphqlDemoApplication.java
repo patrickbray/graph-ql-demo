a7fe44a7af164a324d5270f6610218758892568f
Patrick Bray <pbray@atlassian.com>
2017-07-26 20:41:24 +1000

    Add CF manifest

--- src/main/java/com/pat/graphql/graphqldemo/GraphqlDemoApplication.java
+++ src/main/java/com/pat/graphql/graphqldemo/GraphqlDemoApplication.java
@@ -1,6 +1,5 @@
 package com.pat.graphql.graphqldemo;
 
-import graphql.schema.idl.SchemaParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
